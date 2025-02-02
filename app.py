ba04c86c8143f7ee0b8b1324ec6cd0d256fe3570
Krish200510 <136980489+Krish200510@users.noreply.github.com>
2025-02-02 12:50:42 +0530

    Created database schema for users, subjects, quizzes, and scores

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from models import db, User, Admin, Subject, Chapter, Quiz, Question, Score
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quizmaster.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
